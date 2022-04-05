45c7ae3d43be9a16abb13ac00c646e9911dfc24f
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-04-05 19:55:24 +0000

    update settings.py

--- mealy/settings.py
+++ mealy/settings.py
@@ -30,11 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
-if development:
-    ALLOWED_HOSTS = ["localhost"]
-else:
-    ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
+DEBUG = True
+
+ALLOWED_HOSTS = ["mealy-app.herokuapp.com", "localhost"]
 
 # Application definition
 
