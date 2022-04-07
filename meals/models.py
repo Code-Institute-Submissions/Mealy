1c8c4c76d14f09aee634cde74d581695c8cf17c8
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-04-07 14:51:14 +0000

    migrate db

--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    in_stock = models.BooleanField(default=False)
+
+
+class Meal(models.Model):
+    meal_name = models.CharField(max_length=200, unique=True)
+    planned = models.BooleanField(default=False)
+    ingredients = models.ManyToManyField(Ingredient,blank=True)
+
