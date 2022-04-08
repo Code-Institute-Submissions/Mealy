8e1a4963e8f6d7a11ecef5f7a022e88aa920c4fd
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-04-08 15:43:56 +0000

    add str method to model

--- meals/models.py
+++ meals/models.py
@@ -6,9 +6,15 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     in_stock = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Meal(models.Model):
     meal_name = models.CharField(max_length=200, unique=True)
     planned = models.BooleanField(default=False)
     ingredients = models.ManyToManyField(Ingredient,blank=True)
 
+    def __str__(self):
+        return self.meal_name
